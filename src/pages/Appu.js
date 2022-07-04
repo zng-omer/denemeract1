9e137300f3bd0479c9c639458096e1f1a58c3d7b
Omer ZENGIN <zngzengin7@gmail.com>
2022-07-04 14:26:36 +0300

    watshap tan yazdığım şeyi yaptım

--- src/pages/Appu.js
+++ src/pages/Appu.js
@@ -1,32 +1,9 @@
-import React from 'react';
-
-import {  useEffect, useState } from 'react';
-
 function Appu () {
-  const [surname,setSurname] = useState(()=>{
-    const saved = localStorage.getItem("surname");
-    const initialValu = JSON.parse(saved);
-    return initialValu || "";
-  }
-);
-
-useEffect ( () =>{ 
-    localStorage.setItem("surname",JSON.stringify);
-},[surname]);
-
-
-return(
-  
-  <form class="row g-3">    
-  <div class="col-auto">
-      <label for="staticEmail2" class="visually-hidden">Name:</label>
-      <input type="text"  class="form-control-plaintext" placeholder="fullname" onChange={(e)=>setSurname(e.target.value)} id="staticEmail2" value={surname}></input>
+  return(
+    <div>
+        <h1>Hello my freind0ppppppvlbkvkbvkvbkvbkvkvbbooooooooooooo</h1>
     </div>
-        <div class="col-auto">
-          <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
-        </div>
-      
-  </form>
-);
-}
+  );
+};
+
 export default Appu;
