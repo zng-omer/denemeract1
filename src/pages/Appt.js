7cd448734022d90121ff9cd713c3839bbc4ca494
Omer ZENGIN <zngzengin7@gmail.com>
2022-07-04 12:14:48 +0300

    şu am tekrardan derledim fakat tamamen succefly cevabını aldım fakat nerde hata bulamıyorum

--- src/pages/Appt.js
+++ src/pages/Appt.js
@@ -1,5 +1,5 @@
 import {  useEffect, useState } from 'react';
-
+ 
 
 function Appt (){
 
--- src/pages/Appu.js
+++ src/pages/Appu.js
@@ -1,11 +1,31 @@
 
+import {  useEffect, useState } from 'react';
 
-const Appu = () => {
-  return(
-    <div>
-        <h1>Hello my freind fgnjgdfgdfggg</h1>
+function Appu () {
+  const [surname,setSurname] = useState(()=>{
+    const saved = localStorage.getItem("surname");
+    const initialValu = JSON.parse(saved);
+    return initialValu || "";
+  }
+);
+
+useEffect ( () =>{ 
+    localStorage.setItem("surname",JSON.stringify);
+},[surname]);
+
+
+return(
+  
+  <form class="row g-3">    
+  <div class="col-auto">
+      <label for="staticEmail2" class="visually-hidden">Name:</label>
+      <input type="text"  class="form-control-plaintext" placeholder="fullname" onChange={(e)=>setSurname(e.target.value)} id="staticEmail2" value={surname}></input>
     </div>
-  );
-};
-
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+        </div>
+      
+  </form>
+);
+}
 export default Appu;
