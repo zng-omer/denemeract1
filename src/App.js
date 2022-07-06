49444a8d48c44ba26425c0322215487d667e6a75
Omer ZENGIN <zngzengin7@gmail.com>
2022-07-06 11:35:40 +0300

    covid 19 verilerini çekip oradaki tarihlere göre söylenen tarihin verilerini çıktısını kullandım bunun yanında bootshrap da kullandım App.js te gayet güzel çalışıyor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Appo from "./pages/Appo";
-import Appu from "./pages/Appu";
-import Appt from "./pages/Appt";
-import Layout from "./layout";
-
-
+import axios from 'axios';
+import { useState,useEffect } from "react";
 
 
 function App () {
-      return (
-            <BrowserRouter>
-                  <Routes>
-                        <Route path='/*' element={<Layout />}>  
-                              <Route path='' element={<Appt />} />
-                              <Route path='Appu' element={<Appu />} />
-                              <Route path='Appo' element={<Appo />} />
-                        </Route>
-                     
-                  </Routes>
-            </BrowserRouter>
-      );
+
+    const[veri,setVeri] = useState("");
+    const[tarih,setTarih] =useState("");
+   
+
+    useEffect( () => {
+
+      axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+      .then(respond =>setVeri(respond.data[tarih]))
+      .catch(error => console.log(error))
+
+
+
+    },[veri,tarih]);
+
+
+
+
+  return(
+    <div className='App'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 mx-auto mt-4'>
+            <h2 className='text-center text-white display-3'>Türkiye covid 19 arama motoru</h2>
+            <input type="text" className='form-control' placeholder='GG/AA/YY' value={veri} onChange={(e)=>setTarih(e.target.value)}  />
+            <table class="table" className='table table-striped text-white'>
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Test sayisi</th>
+                  <th scope="col">Hasta sayisi</th>
+                  <th scope="col">Vefat sayisi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='text-white' className={veri === undefined ? "bg-danger" :"bg-success  "}>
+                  <th scope="row" >1</th>
+                  <td>{veri === undefined    ? "Veri Bekleniyor" : veri.totalTests}</td>
+                  <td>{veri === undefined    ? "Veri Bekleniyor" : veri.totalPatients}</td>
+                  <td>{veri === undefined    ? "Veri Bekleniyor" : veri.deaths}</td>
+                </tr>
+              
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+
 };
 export default App; 
